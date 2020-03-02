8d7e88c3079fb7be936d0c4c2416df6fdddef1ec
Aayush Borkar <30978331+Bit145@users.noreply.github.com>
2020-03-02 20:01:59 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_cactus.mk
+    $(LOCAL_DIR)/lineage_cactus.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_cactus-user \
+    lineage_cactus-userdebug \
+    lineage_cactus-eng
